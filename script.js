159b68c037b2df3a316737bf8f5a2f3993143f25
Santhosh1023 <santhoshs0204@gmail.com>
2026-05-08 19:22:07 +0530

    update

---
+++ script.js
@@ -0,0 +1,50 @@
+// let a =10;
+// let b=10;
+// console.log(a+b);
+
+// let firstname=("John");
+// let lastname=("doe");
+// let email=("john@gmail.com");
+
+// console.log(firstname);
+// console.log(lastname);
+// console.log(email);
+
+let students=30;
+// students=students+1;
+// students=students-1;
+// students=students*2;
+// students=students/2;
+// students=students**3;
+// students=students%2;
+
+// post increment
+// students++;
+// post decrement
+// students--;
+
+// pre increment
+// ++students;
+
+// post decrement
+// --students;
+
+// console.log(students);
+
+// let result=1%6+8/2;
+// let result1=6/2**(2+5);
+// console.log(result);
+// console.log(result1);
+
+
+// let username=window.prompt("hello");
+
+// console.log(username);
+
+
+// let username;
+// document.getElementById("mySubmit").onclick=function(){
+//     username=document.getElementById("myText").value;
+//     document.getElementById("myH1").textContent=`${username} welcome to JS`
+// }
+
